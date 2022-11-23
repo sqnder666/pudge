059209865dc8675d980fb1dea7bc58b2b2db7e56
sqnder666 <i.irudovi21@gmail.com>
2022-11-23 21:56:13 +0300

    создание схемы

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-var pu = mongoose.model('pu', { name: String })
+var schema = mongoose.Schema({ name: String })
 
-var pud = new pu({ name: 'Петушок' })
-pud.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('чинчопа')
-    }
+schema.methods.chin = function(){
+    console.log(this.get("name") + " with arcana")
+}
+
+var pud = mongoose.model('pud', schema)
+
+var pu = new pud({ name: 'Pudge' })
+pu.save(function (err) {
+    pu.chin()
 })
