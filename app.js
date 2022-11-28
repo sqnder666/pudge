7464016bc7296144ed2af9b7dff05407e12fadd2
sqnder666 <i.irudovi21@gmail.com>
2022-11-28 20:19:06 +0300

    извлечение данных из бд

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/pudge')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var pudge = require('./routes/pudge');
+var pudges = require('./routes/pudges');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/pudge', pudge);
+app.use('/pudges', pudges);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/pudges.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Новый маршрутизатор, для маршрутов, начинающихся с pudges');
+});
+
+/*Страница пуджей */
+router.get('/:nick', function(req, res, next) {
+    pudge.findOne({nick:req.params.nick}, function(err,pudge){
+        if(err) return next(err)
+        if(!pudge) return next(new Error("Такого Пуджа тут нет, чел ты......"))
+        res.render('pudge', {
+            title: pudge.title,
+            picture: pudge.avatar,
+            desc: pudge.desc
+        })
+    })
+})
+
+
+module.exports = router;
