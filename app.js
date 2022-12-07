9bdceb2984a66c00789c52af640789d94515d8e8
sqnder666 <i.irudovi21@gmail.com>
2022-12-07 20:26:41 +0300

    9.4

--- app.js
+++ app.js
@@ -33,6 +33,10 @@ app.use(session({
   saveUninitialized: true,
   store: MongoStore.create({mongoUrl: 'mongodb://127.0.0.1:27017/pudge'})
 }))
+app.use(function(req,res,next){
+  req.session.counter = req.session.counter +1 || 1
+  next()
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,18 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 var Pudge = require("../models/pudge").Pudge
 
+
 /* GET home page. */
-
-router.get('/', function(req, res, next) {
-    req.session.greeting = "Hi!!!"
-    res.render('index', { title: 'Express', menu:menu });
-
-});
-
-
-
-
-router.get('/', function(req, res, next) {
-    Pudge.find({},{_id:0,title:1,nick:1},function(err,menu){
-        res.render('index', {
-                                title: 'Express',
-                                menu: menu
-                            });
-    })
+router.get('/', function (req, res, next) {
+  Pudge.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
+    req.session.greeting = "HelloWorld!",
+      res.cookie('greeting', 'HelloWorld!').render('index', {
+        title: 'Express',
+        menu: menu,
+        counter:req.session.counter
+      });
+  })
 
 });
 
