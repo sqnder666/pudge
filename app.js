e2de60cbd04fe00444ddd2c60a583cfdf751784d
sqnder666 <i.irudovi21@gmail.com>
2022-12-07 19:21:05 +0300

    9.2

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: "pudge",
+  secret: "Pudsadges",
   cookie:{maxAge:60*1000},
   resave: true,
   saveUninitialized: true	
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ var Pudge = require("../models/pudge").Pudge
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
+});
+
+router.get('/', function(req, res, next) {
   Pudge.find({},{_id:0,title:1,nick:1},function(err,menu){
   res.render('index', { 
       title: 'Express',
