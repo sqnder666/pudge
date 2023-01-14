52b5b79575ca679a17cfe7ab39a66e593dd736ea
sqnder666 <i.irudovi21@gmail.com>
2023-01-14 23:06:45 +0300

    заполнение меню из базы

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,12 +1,14 @@
 var Pudge = require("./../models/pudge").Pudge
 
+
+
 module.exports = function(req,res,next){
-    res.locals.nav = []
+res.locals.nav = []
 
-    Pudge.find(null,{_id:0,title:1,nick:1},function(err,result){
-        if(err) throw err
-        res.locals.nav = result
-        next()
-    })
 
+Pudge.find(null,{_id:0,title:1,nick:1},function(err,result){
+if(err) throw err
+res.locals.nav = result
+next()
+})
 }
